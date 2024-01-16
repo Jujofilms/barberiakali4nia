f12379b09109a93b44c6376d85d426a9f8e9e9d4
Jujofilms <jujomtoma2009@gmail.com>
2024-01-15 19:50:05 -0500

    ok

---
+++ servicios.js
@@ -0,0 +1,13 @@
+document.addEventListener("scroll", function () {
+    let header = document.querySelector(".navbarPrincipal");
+ 
+    if (window.scrollY) {
+       header.style.position = "fixed";
+       header.style.transition = "top";
+       header.style.width = "100%";
+    } else {
+       header.style.position = "static";
+    }
+ });
+ 
+ 
